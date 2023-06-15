2c206f55d8374b1ad4141773795fa2013e21c138
Deepak kumar <dkp0043@gmail.com>
2023-06-15 11:06:47 +0530

    Function Defination

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+//there are two types create function
+
+//1. function defination is a one time process
+
+//2. function defaind
+function myfunction(){//this is not camelCase stander
+    // function Body
+    // I can write many stattements
+
+    console.log("Deepak");//statement/instructions
+}
+
+
+
+function myFunction(){//this is function name camelCase stander
+    //function BOdy
+}
+
+
+
+//3. function calling /invoking  or call/invok
+myfunction();
+myFisrt();
